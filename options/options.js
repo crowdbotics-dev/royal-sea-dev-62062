46f3bf9698ba1bad8dbadc8dce63e21af9d6b077
Crowdbotics <engineering@crowdbotics.com>
2023-02-08 06:01:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-sea-dev-62062",
+  url: "https://royal-sea-dev-62062.botics.co",
+  api: "https://royal-sea-dev-62062.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
